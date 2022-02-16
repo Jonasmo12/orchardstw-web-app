2e6c4b19e3eae7fa43a5e4b63cfb2064634d1af2
Jonas Moloto <jonasmoloto@outlook.com>
2022-02-16 14:11:02 +0200

    fixed collectstatic error on heroku build

--- config/settings/localdev.py
+++ config/settings/localdev.py
@@ -32,6 +32,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -102,10 +105,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = 'static/'
 STATICFILES_DIRS = BASE_DIR / 'static',
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 TEMPLATE_DIRS = BASE_DIR / 'templates',
 
 # Default primary key field type
