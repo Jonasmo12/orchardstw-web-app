63df06864ae8772c77d2a2780b0f3f6af8f13a93
Jonas Moloto <jonasmoloto@outlook.com>
2022-02-17 09:38:45 +0200

    added favicon.icon

--- config/settings/localdev.py
+++ config/settings/localdev.py
@@ -12,7 +12,7 @@ environ.Env.read_env()
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['orchards-web-app.herokuapp.com', '127.0.0.1']
 
